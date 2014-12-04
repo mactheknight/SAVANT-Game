254e2b23485c372914f49d14a63e2e4437c1e381
mactheknight <s650551@student.roundrockisd.org>
2014-12-04 16:05:01 -0600

    third

--- Savant Game.py
+++ Savant Game.py
@@ -16,7 +16,8 @@ inventory = []
     
 def startRoom():
     global inventory
-    print "Entering the dungeon sword in hand, you see a deceased knight slumped against the wall still clutching his shield. It's obviously no use to him anymore..."
+    print "Entering the dungeon sword in hand, you see a deceased knight slumped against the wall still clutching his shield." 
+    print "It's obviously no use to him anymore . . ."
     userInput = raw_input()
     if userInput == "get shield" or userInput == "shield":
         addToInventory("shield")
@@ -49,26 +50,25 @@ if userInput1 == "east":
         print "The only exit remaining is west."
     else:
         print "There is no other option but to turn back."
-    if userInput1 == "west":
-        print "The west room is pitch black, you can't see anything."
-    
-    if userInput2 == "use torch":
-        print "You see a creature, the creature sees you."
-        if userInput2 == "consider":
-            print "You can hear a creature shuffling somewhere within the room. You sneak past him quietly into the next room and a stone door permanently seals the path you came from, leaving you in a room with seemingly no exits."
-            if userInput1 == "up":
-                print "Looking desperately for an exit you glance and notice a rope hanging down from the pitch black abyss that is the cieling."
-                print "Your only chance of escape is to climb the rope, and so you do."
-                print "After pulling yourself up the rope with your noodle arms for what seems like forever you reach an opening in the wall which you lean over to and abandon the rope."
-                print "The final room is another pitch black room. However this time you hear the tingling of coins or something metallic as they rustle and bounce off of the stone floors."
-                print "Fire illuminates the room, straight from the jaw of Emrys the ancient and devious dragon."
-                print "You spot Princess Amber from across the great hall of loot and gems that is the den Emrys resides in."
-                print "She is unconscious. Before you can make any movement, everything becomes pitch black once again as you have been snatched into the jaws of Emrys, the shield proving useless and realisticaly teaching you the valuable life lesson that a human is no match for a dragon. Pfsh, you weren't even a wizard."
-                print "GAME OVER."
-            else: 
-                print "There are no exits !"
-        
+if userInput1 == "west":
+    print "The west room is pitch black, you can't see anything."
+if userInput2 == "use torch":
+    print "You see a creature, the creature sees you."
+    if userInput2 == "consider":
+        print "You can hear a creature shuffling somewhere within the room. You sneak past him quietly into the next room and a stone door permanently seals the path you came from, leaving you in a room with seemingly no exits."
+        if userInput1 == "up":
+            print "Looking desperately for an exit you glance and notice a rope hanging down from the pitch black abyss that is the cieling."
+            print "Your only chance of escape is to climb the rope, and so you do."
+            print "After pulling yourself up the rope with your noodle arms for what seems like forever you reach an opening in the wall which you lean over to and abandon the rope."
+            print "The final room is another pitch black room. However this time you hear the tingling of coins or something metallic as they rustle and bounce off of the stone floors."
+            print "Fire illuminates the room, straight from the jaw of Emrys the ancient and devious dragon."
+            print "You spot Princess Amber from across the great hall of loot and gems that is the den Emrys resides in."
+            print "She is unconscious. Before you can make any movement, everything becomes pitch black once again as you have been snatched into the jaws of Emrys, the shield proving useless and realisticaly teaching you the valuable life lesson that a human is no match for a dragon. Pfsh, you weren't even a wizard."
+            print "GAME OVER."
         else: 
-            print "The creature kills you !"
-            print "GAME OVER."    
+            print "There are no exits !"
+        
+    else: 
+        print "The creature kills you !"
+        print "GAME OVER."    
     
